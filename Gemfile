8606a451b7eecdebbaff3887c82d987a181bc608
Astro Lee <leonardo.lee1993@gmail.com>
2018-08-20 15:36:45 +0900

    image-server sinatra

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem 'sinatra'
+gem 'sinatra-cross_origin'
+gem 'aws-sdk-s3', '~> 1'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'json'
---
+++ app.rb
@@ -0,0 +1,73 @@
+require 'sinatra'
+require 'sinatra/cors'
+require 'aws-sdk-s3'
+require 'dotenv'
+require 'securerandom'
+
+# Configs
+Dotenv.load
+Aws.config.update({
+  region: 'us-west-2',
+  credentials: Aws::Credentials.new("#{ENV['S3_ACCESS_KEY']}", "#{ENV['S3_SECRET_KEY']}")
+})
+set :allow_origin, Sinatra::Base.production? ? "https://steemhunt.com" : "http://localhost:3000 http://localhost:4567"
+set :allow_methods, "GET,HEAD,POST"
+set :allow_headers, "content-type,if-modified-since"
+set :expose_headers, "location,link"
+
+# Routes
+get '/upload' do
+  "
+  <form action='/upload' method='post' enctype='multipart/form-data'>
+    <input type='file' name='image' value='image-file'></input>
+    <input type='submit'/>
+  </form>
+  "
+end
+
+post '/upload' do
+  content_type :json
+  uploader = S3Uploader.new('huntimages')
+
+  if res = uploader.upload(params[:image])
+    res
+  else
+    status 500
+  end
+end
+
+# Modules
+class S3Uploader
+  attr_accessor :bucket_name, :s3, :bucket, :space, :name, :path, :link
+
+  def initialize(bucket_name)
+    @bucket_name = bucket_name
+    @s3 = Aws::S3::Resource.new
+    @bucket = s3.bucket(bucket_name)
+  end
+
+  def upload(image)
+    @name = image[:filename]
+    @path = "images/#{SecureRandom.hex(16)}-#{name}"
+    @path.prepend Sinatra::Base.production? ? "production/" : "development/"
+    @link = "https://s3-us-west-2.amazonaws.com/#{bucket_name}/#{path}"
+    @space = bucket.object(path)
+    if space.upload_file(image[:tempfile], acl:'public-read')
+      return render_json
+    else
+      return false
+    end
+  end
+
+  private
+
+  def render_json
+    {
+      data: {
+        name: name, link: link
+      },
+      success: true,
+      status: 200
+    }.to_json
+  end
+end
